7d9c89730564c8c53ae535b3f8819de671dc1bf5
unknown <nehamahajan4665@gmail.com>
2024-03-04 18:12:38 +0530

    3/4/24

--- src/main/java/com/hcms/controller/EmployeeController.java
+++ src/main/java/com/hcms/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package com.hcms.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hcms.entities.Employees;
+import com.hcms.services.EmployeesService;
+
+@RestController
 public class EmployeeController {
+	
+	@Autowired
+	private EmployeesService employeeService;
+	
+	@GetMapping("/employees")
+	private ResponseEntity<List<Employees>>getAll(){
+		return new ResponseEntity<List<Employees>>(employeeService.getAllEmployees(),HttpStatus.OK);
+	}
+	
 
 }
